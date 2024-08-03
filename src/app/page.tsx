c14656c2df841a2be1e0e1cad9fd0ba5732686dd
Freelancer Jember <depadeh@hotlmail.com>
2024-08-03 18:00:20 +0700

    Change title landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 export default function Beranda() {
   return (
-    <p>Another Portfolio</p>
+    <main className="flex h-screen">
+      <div className="m-auto">
+        <h1 className="text-3xl font-extrabold">Another Portfolio</h1>
+      </div>
+    </main>
   );
 }
