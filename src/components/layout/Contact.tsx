08adab075797558fc76bfd1f38f0feb33c9327e9
Freelancer Jember <depadeh@hotlmail.com>
2024-08-08 19:55:42 +0700

    Contact Component in Landing Page

--- src/components/layout/Contact.tsx
+++ src/components/layout/Contact.tsx
@@ -1,5 +1,35 @@
+import {Button, Input, Textarea} from "@nextui-org/react";
+import React from "react";
+
 export function Contact() {
   return (
-    <><p>Contact</p></>
+    <section className="flex flex-row w-full justify-center py-8">
+      <div className="w-2/5">
+        <h1 className="text-xl font-semibold uppercase text-center mb-4 tracking-wider">
+          Contact Me
+        </h1>
+        <span className="w-full flex flex-col gap-4">
+          <Input
+            type="text"
+            label="Full name"
+            placeholder="Enter your full name"
+          />
+          <Input type="email" label="Email" placeholder="Enter a valid email"/>
+          <Textarea
+            type="text"
+            label="Message"
+            placeholder="Enter your messages"
+            maxRows={5}
+          />
+          <Button
+            variant="solid"
+            color="primary"
+            className="text-sm font-medium uppercase tracking-widest"
+          >
+            Send
+          </Button>
+        </span>
+      </div>
+    </section>
   );
 }
